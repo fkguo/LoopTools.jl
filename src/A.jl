77ca7de3cc076e377faabbd265254a5849ea0577
fkguo <62633089+fkguo@users.noreply.github.com>
2020-08-09 23:49:11 +0800

    Update A.jl

--- src/A.jl
+++ src/A.jl
@@ -43,7 +43,7 @@ the scalar one-point one-loop function
 
 ```math
 \frac{μ^{4-D}}{iπ^{D/2} r_Γ} \int d^D q \frac{1}{q^2-m^2}
-````
+```
 
 with ``r_Γ = \frac{Γ^2(1-ε)Γ(1+ε)}{Γ(1-2ε)}``, ``D=4-2ε``.
 """
