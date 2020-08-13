c2938c634e4bab1b5fb2ec1b2205d109b0387f03
Feng-Kun Guo <fkguo@itp.ac.cn>
2020-08-13 14:26:29 +0800

    doc update

--- src/E.jl
+++ src/E.jl
@@ -33,15 +33,18 @@ const ecoef = (:ee0, :ee1, :ee2, :ee3, :ee4, :ee00, :ee11, :ee12, :ee13, :ee14,
 
 
 @doc raw"""
-    E0i(id, p1^2, p2^2, p3^2, p4^2, p5^2, (p1+p2)^2, (p2+p3)^2, (p3+p4)^2, (p4+p5)^2, (p5+p1)^2, m1^2, m2^2, m3^2, m4^2, m5^2)
+    E0i(id, p1^2, p2^2, p3^2, p4^2, p5^2, (p1+p2)^2, (p2+p3)^2, (p3+p4)^2, (p4+p5)^2, 
+    (p5+p1)^2, m1^2, m2^2, m3^2, m4^2, m5^2)
 
 five-point tensor coefficient for `id`
 
 ```math
-\frac{μ^{4-D}}{iπ^{D/2} r_Γ} \int
-\frac{({\rm numerator})\, d^D q }{(q^2-m_1^2)\left[(q+p_1)^2-m_2^2\right]
-\left[(q+p_1+p_2)^2-m_3^2\right] \left[(q+p_1+p_2+p_3)^2-m_4^2\right]
- \left[(q+p_1+p_2+p_3+p_4)^2-m_5^2\right]}
+\begin{aligned}
+\frac{μ^{4-D}}{iπ^{D/2} r_Γ} \int d^D q &
+\frac{({\rm numerator}) }{(q^2-m_1^2)\left[(q+p_1)^2-m_2^2\right]
+\left[(q+p_1+p_2)^2-m_3^2\right] \left[(q+p_1+p_2+p_3)^2-m_4^2\right]} \\
+&\times \frac1{\left[(q+p_1+p_2+p_3+p_4)^2-m_5^2\right]}
+ \end{aligned}
 ```
 
 with ``r_Γ = \frac{Γ^2(1-ε)Γ(1+ε)}{Γ(1-2ε)}``, ``D=4-2ε``.
@@ -84,15 +87,18 @@ end
 
 
 @doc raw"""
-    E0(p1^2, p2^2, p3^2, p4^2, p5^2, (p1+p2)^2, (p2+p3)^2, (p3+p4)^2, (p4+p5)^2, (p5+p1)^2, m1^2, m2^2, m3^2, m4^2, m5^2)
+    E0(p1^2, p2^2, p3^2, p4^2, p5^2, (p1+p2)^2, (p2+p3)^2, (p3+p4)^2, (p4+p5)^2, 
+    (p5+p1)^2, m1^2, m2^2, m3^2, m4^2, m5^2)
 
 the scalar five-point one-loop function
 
 ```math
-\frac{μ^{4-D}}{iπ^{D/2} r_Γ} \int
-\frac{d^D q }{(q^2-m_1^2)\left[(q+p_1)^2-m_2^2\right]
-\left[(q+p_1+p_2)^2-m_3^2\right] \left[(q+p_1+p_2+p_3)^2-m_4^2\right]
-\left[(q+p_1+p_2+p_3+p_4)^2-m_5^2\right]}
+\begin{aligned}
+\frac{μ^{4-D}}{iπ^{D/2} r_Γ} \int d^D q &
+\frac{1  }{(q^2-m_1^2)\left[(q+p_1)^2-m_2^2\right]
+\left[(q+p_1+p_2)^2-m_3^2\right] \left[(q+p_1+p_2+p_3)^2-m_4^2\right]} \\
+&\times \frac1{\left[(q+p_1+p_2+p_3+p_4)^2-m_5^2\right]}
+\end{aligned}
 ```
 
 with ``r_Γ = \frac{Γ^2(1-ε)Γ(1+ε)}{Γ(1-2ε)}``, ``D=4-2ε``.
