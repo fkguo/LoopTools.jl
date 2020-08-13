2c224f217492a0d89354d7e0e9f62fbafc47375f
Feng-Kun Guo <fkguo@itp.ac.cn>
2020-08-13 23:29:55 +0800

    w/ new version of SymbolicUtils

--- src/sym.jl
+++ src/sym.jl
@@ -2,16 +2,13 @@ using SymbolicUtils
 
 @syms ε::Real
 
-# rule0 = @rule 0/~ε => 0
-
 @doc raw"""
     DRResult(c0, c1, c2)
 
 arrange the coefficients of ``1/ε``, corresponding to the `Mathematica` version of the same name.
 `1/ε` corresponds to `DR1eps` in the `Mathematica` version.
 """
-DRResult(c0::Real, c1::Real, c2::Real)::SymbolicUtils.Term{Real} = (c0 + c1/ε + c2/ε^2)
-DRResult(c0, c1, c2)::SymbolicUtils.Term{Complex{Real}} = (c0 + c1/ε + c2/ε^2)
+DRResult(c0, c1, c2) = simplify(c0 + c1/ε + c2/ε^2)
 
 function _round(x; kws...)
     imag(x) < sqrt(eps(1.0)) && (x = real(x))
